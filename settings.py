2bd66f84779d45f8d72d27a0babad350c18fc94e
Kariyushi <kariyushi@uchicago.edu>
2017-12-05 21:42:41 -0600

    fifth try

--- settings.py
+++ settings.py
@@ -118,12 +118,32 @@ mturk_hit_settings = {
     'preview_template': 'global/MTurkPreview.html',
     'minutes_allotted_per_assignment': 10,
     'expiration_hours': 1*24, # 7 days
-    'grant_qualification_id': '3TBOPHETGJKON9TEO7236BKDIO22LX',# to prevent retakes (sandbox id)
+    # flags workers once they've completed the task (sandbox ID)
+    'grant_qualification_id': '3TBOPHETGJKON9TEO7236BKDIO22LX',
     'qualification_requirements': [
-        qualification.LocaleRequirement("EqualTo", "US"),
-        qualification.PercentAssignmentsApprovedRequirement("GreaterThanOrEqualTo", 95),
-        qualification.NumberHitsApprovedRequirement("GreaterThanOrEqualTo", 5),
-        qualification.Requirement('3TBOPHETGJKON9TEO7236BKDIO22LX', 'DoesNotExist')
+        # prevents workers from completing again after receiving flag
+        {
+            'QualificationTypeID': "3TBOPHETGJKON9TEO7236BKDIO22LX",
+            'Comparator': "DoesNotExist",
+        },
+        # requires workers be located in the US
+        {
+            'QualificationTypeID': "00000000000000000071",
+            'Comparator': "EqualTo",
+            'LocaleValues': [{'Country': "US"}]
+        },
+        # requires workers to have completed at least 5 HITs
+        {
+            'QualificationTypeID': "00000000000000000040",
+            'Comparator': "GreaterThan",
+            'IntegerValues': [5]
+        },
+        # requires workers to have HIT approval rate of at least 95 percent
+        {
+            'QualificationTypeID': "000000000000000000L0",
+            'Comparator': "GreaterThanOrEqualTo",
+            'IntegerValues': [95]
+        },
     ]
 }
 
